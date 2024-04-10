6a59cd0a68216c1d834e41e08ac973f09fbca846
vali <valentin@kaas.cc>
2024-04-10 14:21:41 +0200

    added sane modules

---
+++ options/desktop/fonts.nix
@@ -0,0 +1,13 @@
+{ pkgs, ... }:
+{
+  fonts.packages = with pkgs; [
+    material-design-icons
+    (nerdfonts.override {
+        fonts = [ "JetBrainsMono"];
+      })
+    noto-fonts
+    noto-fonts-cjk-sans
+    noto-fonts-cjk-serif
+    noto-fonts-emoji
+  ];
+}
