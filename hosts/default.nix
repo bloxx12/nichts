30d400d8af6f58278456959d407682a32cf31cd2
vali <valentin@kaas.cc>
2024-04-10 17:56:10 +0200

    added overridden defaults.nix again

--- hosts/default.nix
+++ hosts/default.nix
@@ -1,8 +1,15 @@
-_: {
-    imports = [
-      ./configuration.nix
-      ./programs.nix
-      ./hardware-configuration.nix
-      ./profile.nix
-    ];
-}
+{ inputs, ... }:
+let 
+  inherit (inputs) self;
+  inherit (self) lib;
+in {
+  vali = lib.nixosSystem {
+    system = "x86_64-linux";
+    specialArgs = { inherit lib inputs self; };
+  };
+  modules = [
+      ./vali
+      ../modules/vali
+      inputs.home-manager.noxosModules.home-manager
+      inputs.agenix.noxosModules.default
+  ];
