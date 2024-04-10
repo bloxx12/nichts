86e93de371b3c6a34cbb37ce4b47eca4dd3c98e3
vali <valentin@kaas.cc>
2024-04-10 11:35:09 +0200

    set up something working (hopefully)

---
+++ hosts/vali/desktop/default.nix
@@ -0,0 +1,16 @@
+{ inputs, outputs, pks, lib, profile-config, ... }:
+{
+  imports = [
+  ../../../modules/vali/default.nix
+  ../common
+  ./hardware-configuration.nix
+  ./packages.nix
+  ./boot.nix
+  ];
+  i3.enable = true; 
+  security.polkit.enable = true;
+  # Set the keyboard layout to DE
+  services.xserver.xkb.layout.enable = "de";
+  console.keyMap = "de";
+}
+
