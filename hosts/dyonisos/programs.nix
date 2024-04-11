adb54eafba221cde9d0f93fe77c4675ef76c8d39
get <wasser.ian@gmail.com>
2024-04-11 22:14:55 +0200

    Added dyonisos host

---
+++ hosts/dyonisos/programs.nix
@@ -0,0 +1,50 @@
+{ config, inputs, pkgs, ... }:
+let
+  username = config.myOptions.other.system.username;
+in {
+    home-manager.users.${username} = {
+        home.packages = let
+          fenix = inputs.fenix.packages.${pkgs.system};
+
+        in with pkgs; [
+	    neofetch
+            git
+	    trash-cli
+            element-desktop
+            steam
+            libreoffice-fresh
+            ventoy-full
+            lazygit
+	    # obsidian
+            neofetch
+            zip
+            vlc
+            zathura
+            alacritty
+            unzip
+            gcc
+            bibata-cursors
+            networkmanagerapplet
+            xclip
+            pamixer
+            pcmanfm
+            ffmpeg_6-full
+            (fenix.complete.withComponents [
+              "cargo"
+              "clippy"
+              "rust-src"
+              "rustc"
+              "rustfmt"
+            ])
+            polkit
+            fastfetch
+            alsa-utils
+            gdb
+            tree
+            smartmontools
+            python3
+            rustdesk
+            neovim
+        ];
+    };
+}
