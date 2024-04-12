0a2719087166c0d376f42cafa17776cb679136c7
vali <valentin@kaas.cc>
2024-04-12 14:54:23 +0200

    fixed zsh.nix, fixed mars directory profile

--- hosts/vali/mars/profile.nix
+++ hosts/vali/mars/profile.nix
@@ -1,9 +1,9 @@
 _: {
     imports = [
-        ../../options/boot/grub-boot.nix
-        ../../options/desktop/fonts.nix
-        ../../options/common/networking.nix
-        ../../options/common/gpu/nvidia.nix
+        ../../../options/boot/grub-boot.nix
+        ../../../options/desktop/fonts.nix
+        ../../../options/common/networking.nix
+        ../../../options/common/gpu/nvidia.nix
 #        ../../options/common/pin-registry.nix
 #        ../../options/common/preserve-system.nix
         #../../options/
