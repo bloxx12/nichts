381f9add94c1f035f46fcc4bb793d453f70b4bb6
vali <valentin@kaas.cc>
2024-04-12 19:22:28 +0200

    added an overlay, the foot terminal and hypr stuff

--- flake.nix
+++ flake.nix
@@ -7,13 +7,21 @@
     };
     inputs = {
         nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+        nixpkgs-wayland.url = "github:nix-community/nixpkgs-wayland";
+        anyrun = {
+            url = "github:Kirottu/anyrun";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
 
         home-manager = {
             url = "github:nix-community/home-manager";
             inputs.nixpkgs.follows = "nixpkgs";
         };
 
-        nur.url = "github:nix-community/NUR";
+        nur = {
+            url = "github:nix-community/NUR";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
 
         agenix.url = "github:ryantm/agenix";
 
@@ -27,11 +35,20 @@
         fenix = {
             url = "github:nix-community/fenix";
             inputs.nixpkgs.follows = "nixpkgs";
-          };
+        };
 	
         hyprland = {
             url = "github:hyprwm/Hyprland";
             inputs.nixpkgs.follows = "nixpkgs";
+        };
+        hyprlock = {
+            url = "github:hyprwm/hyprlock";
+            inputs.nixpkgs.follows = "nixpkgs";
           };
+
+        split-monitor-workspaces = {
+            url = "github:Duckonaut/split-monitor-workspaces";
+            inputs.hyprland.follows = "hyprland";
+        };
     };
 }
