dc3942a8384ee8bf4f3d8778020cbf82afaa36c5
LarsZauberer <wasser.ian@gmail.com>
2024-04-12 15:53:06 +0200

    Minimal hyprland config

--- hosts/lars/kronos/configuration.nix
+++ hosts/lars/kronos/configuration.nix
@@ -7,5 +7,11 @@
               username = "lars";
           };
       };
+      hyprland = {
+          enable = true;
+      	  nvidia.enable = true;
+      };
   };
+
+  services.getty.autologinUser = "lars";
 }
--- hosts/lars/kronos/default.nix
+++ hosts/lars/kronos/default.nix
@@ -1,6 +1,12 @@
 _: {
     imports = [
       ../.
+      ./../programs.nix
+      ./../hyprland.nix
+      ./programs.nix
+      ./configuration.nix
+      ./hardware-configuration.nix
+      ./profile.nix
       ./configuration.nix
       ./programs.nix
       ./hardware-configuration.nix
