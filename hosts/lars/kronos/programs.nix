e8c795a93c7fdc11de493ac20717a734f0bc86da
get <wasser.ian@gmail.com>
2024-04-12 10:34:06 +0200

    globalized for lars programs.nix

--- hosts/lars/kronos/programs.nix
+++ hosts/lars/kronos/programs.nix
@@ -7,44 +7,6 @@ in {
           fenix = inputs.fenix.packages.${pkgs.system};
 
         in with pkgs; [
-	    neofetch
-            git
-	    trash-cli
-            element-desktop
-            steam
-            libreoffice-fresh
-            ventoy-full
-            lazygit
-	    # obsidian
-            neofetch
-            zip
-            vlc
-            zathura
-            alacritty
-            unzip
-            gcc
-            bibata-cursors
-            networkmanagerapplet
-            xclip
-            pamixer
-            pcmanfm
-            ffmpeg_6-full
-            (fenix.complete.withComponents [
-              "cargo"
-              "clippy"
-              "rust-src"
-              "rustc"
-              "rustfmt"
-            ])
-            polkit
-            fastfetch
-            alsa-utils
-            gdb
-            tree
-            smartmontools
-            python3
-            rustdesk
-            neovim
         ];
     };
 }
