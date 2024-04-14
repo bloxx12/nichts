ed6e54a11e186cd79a4165d6b92f0059ec1727b9
Dragyx <66752602+Dragyx@users.noreply.github.com>
2024-04-14 22:54:16 +0200

    update theming of hosts/dragyx

--- hosts/dragyx/common/configuration.nix
+++ hosts/dragyx/common/configuration.nix
@@ -1,6 +1,19 @@
 { config, pkgs, ... }:
 
 {
+  imports = [
+    ../../../options/common/pin-registry.nix
+    ../../../options/common/preserve-system.nix
+    ../../../options/desktop/fonts.nix
+  ];
+
+
+  services.locate = {
+    enable = true;
+    interval = "hourly";
+    package = pkgs.plocate;
+    localuser = null;
+  };
 
 
 }
