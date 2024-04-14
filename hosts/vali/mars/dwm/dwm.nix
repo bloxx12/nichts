e5fdc3585eb1e6f4b9a5e51592d5e06f0ec26ff3
vali <valentin@kaas.cc>
2024-04-14 11:39:54 +0200

    added st

--- hosts/vali/mars/dwm/dwm.nix
+++ hosts/vali/mars/dwm/dwm.nix
@@ -6,18 +6,22 @@ in {
   options.modules.programs.dwm.enable = mkEnableOption "dwm";
 
   config = mkIf cfg.enable {
-    services.xserver = {
+        services.xserver = {
       enable = true;
       xkb.layout =  "de";
       windowManager.dwm = {
           enable = true;
-   #       package = pkgs.dwm.overrideAttrs { src = ./dwm-6.5/dwm; };
       };
     };
     nixpkgs.overlays = [
     (final: prev: {
-      dwm = prev.dwm.overrideAttrs (old: {src = ./dwm-6.5;}); #FIX ME: Update with path to your dwm folder
+      dwm = prev.dwm.overrideAttrs (old: {src = ./dwm-6.5;});
+      dmenu = prev.dmenu.overrideAttrs (old: {src = ./dmenu-5.3;});
     })
     ];
+    environment.systemPackages = with pkgs; [
+      (st.overrideAttrs (oldAttrs: rec { src = ./st-0.9.2; }))
+    ];
+
   };
 }
