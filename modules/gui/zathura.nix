e289544df2e585ea3e0a22b834f01264f5fdcd8f
Dragyx <66752602+Dragyx@users.noreply.github.com>
2024-04-19 21:03:12 +0200

    Pin zathura catpuccin theme to version

--- modules/gui/zathura.nix
+++ modules/gui/zathura.nix
@@ -6,16 +6,19 @@
 }: with lib; let
     cfg = config.modules.programs.zathura;
     username = config.modules.other.system.username;
+    catppuccin = pkgs.fetchFromGitHub {
+      owner = "catppuccin";
+      repo = "zathura";
+      rev = "4eb02fd206de63b2423f6deb58242d352545b52f";
+      hash = "sha256-/vD/hOi6KcaGyAp6Az7jL5/tQSGRzIrf0oHjAJf4QbI=";
+    };
+
 in {
     options.modules.programs.zathura.enable = mkEnableOption "zathura";
 
     config = mkIf cfg.enable {
         home-manager.users.${username} = {
-            xdg.configFile."zathura/catppuccin-mocha".source = pkgs.fetchurl {
-              url = "https://raw.githubusercontent.com/catppuccin/zathura/main/src/catppuccin-mocha";
-              hash = "sha256-/HXecio3My2eXTpY7JoYiN9mnXsps4PAThDPs4OCsAk=";
-            };
-
+            xdg.configFile."zathura/catppuccin-mocha".source = "${catppuccin}/src/catppuccin-mocha";
             programs.zathura = {
                 enable = true;
                 extraConfig = ''
