7e11feec0d5a5ad6ac5978c0b8bfc9620a7eecb0
Dragyx <66752602+Dragyx@users.noreply.github.com>
2024-04-21 20:09:31 +0200

    Remove WIP vivado 2019.2

--- hosts/dragyx/flocke/packages.nix
+++ hosts/dragyx/flocke/packages.nix
@@ -14,6 +14,7 @@ in
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    # inputs.vivado2019flake.vivado-2019_2
 
     # security audits
     lynis
