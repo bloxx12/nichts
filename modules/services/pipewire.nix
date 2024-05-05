0022cf7d503da2b71f721caa6f0e9d93319289ef
Charlie Root <charlie@charlieroot.dev>
2024-05-05 22:12:18 +0200

    added anyrun config

--- modules/services/pipewire.nix
+++ modules/services/pipewire.nix
@@ -8,10 +8,7 @@ in {
     hardware.pulseaudio.enable = false;
     services.pipewire = {
       enable = true;
-      alsa.enable = true;
-      alsa.support32Bit = true;
       pulse.enable = true;
-      wireplumber.enable = true;
     };
     security.rtkit.enable = true;
   };
