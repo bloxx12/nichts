a7ff1915613ad5e6118b7e3e92c784b8bb3b0a60
Charlie Root <charlie@charlieroot.dev>
2024-05-23 11:38:43 +0200

    added laptop

---
+++ hosts/vali/hermit/profile.nix
@@ -0,0 +1,9 @@
+_: {
+  imports = [
+    ../../../options/boot/grub-boot.nix
+    ../../../options/desktop/fonts.nix
+    ../../../options/common/networking.nix
+    ../../../options/desktop/monitors.nix
+    ../../../overlay.nix
+  ];
+}
