6b8394bb202b11b6166a17b8e6e608e969c8e7e3
Charlie Root <charlie@charlieroot.dev>
2024-05-29 11:46:43 +0200

    hyprland revert to prev commit

--- modules/wms/wayland/variables.nix
+++ modules/wms/wayland/variables.nix
@@ -15,26 +15,19 @@ in {
       NIXOS_OZONE_WL = "1";
       __GL_GSYNC_ALLOWED = "0";
       __GL_VRR_ALLOWED = "0";
-      # _JAVA_AWT_WM_NONEREPARENTING = "1";
-      # SSH_AUTH_SOCK = "/run/user/1000/keyring/ssh";
       DISABLE_QT5_COMPAT = "0";
       GDK_BACKEND = "wayland,x11";
       ANKI_WAYLAND = "1";
       DIRENV_LOG_FORMAT = "";
       WLR_DRM_NO_ATOMIC = "1";
       QT_AUTO_SCREEN_SCALE_FACTOR = "1";
-      QT_QPA_PLATFORM = "wayland;xcb";
+      QT_QPA_PLATFORM = "wayland";
       DISABLE_QT_COMPAT = "0";
       QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
       MOZ_ENABLE_WAYLAND = "1";
-      # WLR_BACKEND = "vulkan";
-      # WLR_RENDERER = "vulkan";
       XDG_SESSION_TYPE = "wayland";
       SDL_VIDEODRIVER = "wayland";
       CLUTTER_BACKEND = "wayland";
-      # WLR_DRM_DEVICES = "/dev/dri/card1:/dev/dri/card0";
-      # };
-      # }
       # Session variables for Hyprland
       # (lib.mkIf (hyprland.enable) {
       # environment.variables = {
@@ -45,10 +38,8 @@ in {
       XDG_SESSION_DESKTOP = "Hyprland";
       ELECTRON_OZONE_PLATFORM_HINT = "auto";
       WLR_RENDERER_ALLOW_SOFTWARE = "1";
-      # QT_QPA_PLATFORM = "wayland";
       # Needed anymore?
       LIBSEAT_BACKEND = "logind";
-      # WLR_NO_HARDWARE_CURSORS = "1";
     };
     # })
     # ];
