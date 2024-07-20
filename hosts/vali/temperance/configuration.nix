f26423354f1ae81f76385e6f245e1bea43314ec1
Vali <valentin@kaas.cc>
2024-07-20 08:14:46 +0200

    temperance: Fixed configuration.nix

--- hosts/vali/temperance/configuration.nix
+++ hosts/vali/temperance/configuration.nix
@@ -1,4 +1,8 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  config,
+  ...
+}: {
   # allow unfree packages
   nixpkgs.config.allowUnfree = true;
   # Time Zone
@@ -16,17 +20,15 @@
       experimental-features = ["nix-command" "flakes"];
       # reduce file size used & automatic garbage collector
       auto-optimise-store = true;
+      max-jobs = 3;
+      cores = 4;
     };
   };
   security.sudo.package = pkgs.sudo.override {withInsults = true;};
   security.polkit.enable = true;
   programs.kdeconnect.enable = true;
   programs.dconf.enable = true;
-  # boot.kernelModules = [ "v4l2loopback" ];
-  boot.kernelPackages = pkgs.linuxPackages_xanmod_stable;
-  # boot.extraModulePackages = [ pkgs.linuxPackages.v4l2loopback ];
-  #  services.picom.enable = true;
-  #  services.emacs.enable = true;
+  boot.kernelPackages = pkgs.linuxPackages_xanmod_latest;
   modules = {
     other = {
       system = {
@@ -40,21 +42,21 @@
       };
     };
     wms = {
-      x = {
-        i3.enable = false;
-      };
       wayland = {
         enable = true;
         hyprland.enable = true;
       };
     };
     programs = {
+      beets.enable = true;
       vesktop.enable = true;
       ssh.enable = true;
       btop.enable = true;
       mpv.enable = true;
       kitty.enable = true;
       newsboat.enable = true;
+      ncmpcpp.enable = true;
+      wtfutil.enable = true;
       foot.enable = true;
       fish.enable = true;
       stylix.enable = true;
@@ -63,22 +65,36 @@
       spicetify.enable = true;
       schizofox.enable = true;
       anyrun.enable = true;
+      zathura.enable = true;
     };
     editors = {
-      emacs = {
-        enable = false;
-        doom.enable = false;
-      };
+      emacs.enable = true;
       helix.enable = true;
       kakoune.enable = true;
-      nixvim.enable = false;
+      neovim.enable = true;
     };
     services = {
       pipewire.enable = true;
       dunst.enable = true;
+      mpd = {
+        enable = true;
+        musicDirectory = "/home/${config.modules.other.system.username}/Nextcloud/Media/Music";
+      };
     };
-    themes = {
-      gtk = {enable = true;};
+    theming = {
+      quickshell.enable = true;
+      stylix = {
+        enable = true;
+        scheme = "${pkgs.base16-schemes}/share/themes/tomorrow-night.yaml";
+        cursor.size = 28;
+        fontsizes = {
+          terminal = 18;
+          popups = 14;
+          applications = 14;
+        };
+        image = ../../../assets/wallpapers/tiredgod.png;
+      };
+      gtk = {enable = false;};
       qt = {
         enable = true;
         package = pkgs.kde-gruvbox;
