ba3b827e73c890fa5a1edc6481c78b57d79acac3
Charlie Root <charlie@charlieroot.dev>
2024-07-30 19:24:53 +0200

    switching to helix once again

---
+++ modules/system/keyboard.nix
@@ -0,0 +1,8 @@
+{
+  services.xserver = {
+    layout = "de,us";
+    xkbVariant = ",cmk_ed_dh";
+    xkbModel = ",pc105awide";
+    xkbOptions = ",misc:extend,lv5:caps_switch_lock,compose:menu";
+  };
+}
