bf0174978e02427926efed675b811a01a0723a23
Charlie Root <charlie@charlieroot.dev>
2024-08-13 11:09:42 +0200

    removing settings

--- modules/gui/minecraft.nix
+++ modules/gui/minecraft.nix
@@ -5,7 +5,7 @@
   ...
 }:
 with lib; let
-  username = config.modules.other.system.username;
+  inherit (config.modules.other.system) username;
   cfg = config.modules.programs.minecraft;
 in {
   options.modules.programs.minecraft = {
@@ -14,15 +14,6 @@ in {
   };
 
   config = mkIf cfg.enable {
-    # Set wayland environment flag
-    environment.variables =
-      mkIf cfg.wayland {__GL_THREADED_OPTIMIZATIONS = 0;};
-    # Install glfw-wayland-minecraft
-    environment.systemPackages = with pkgs;
-      mkIf cfg.wayland [
-        glfw-wayland-minecraft # Use these parameters in the prism launcher: -Dfml.earlyprogresswindow=false -Dorg.lwjgl.glfw.libname=/nix/store/ypkdx5844pp1vdw2z2nmnf2nb9kgl0mp-glfw-wayland-minecraft-unstable-2023-06-01/lib/libglfw.so
-      ];
-
     home-manager.users.${username} = {
       # Install minecraft
       home.packages = with pkgs; [prismlauncher];
