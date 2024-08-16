70641598f9f85ff6c893c7529d9ed8e24cbcaa38
Charlie Root <charlie@charlieroot.dev>
2024-08-16 16:34:09 +0200

    more changes

--- modules/system/nix/nixpkgs.nix
+++ modules/system/nix/nixpkgs.nix
@@ -8,14 +8,10 @@
     # <https://nixos.org/manual/nixpkgs/unstable/#chap-packageconfig>
     hostPlatform = "x86_64-linux";
     config = {
-      # Allow broken packages to be built. Setting this to false means packages
-      # will refuse to evaluate sometimes, but only if they have been marked as
-      # broken for a specific reason. At that point we can either try to solve
-      # the breakage, or get rid of the package entirely.
-      allowBroken = false;
+      # Disallow broken packages to be built.       allowBroken = false;
       allowUnsupportedSystem = true;
 
-      # Allow unfree packages since it's sadly necessary
+      # Allow unfree packages
       allowUnfree = true;
 
       # Default to none, add more as necessary. This is usually where
@@ -27,11 +23,6 @@
       # and the description for this option recommends this to be true, I prefer
       # explicit naming conventions, i.e., no aliases.
       allowAliases = false;
-
-      # Enable parallel building by default. This, in theory, should speed up building
-      # derivations, especially rust ones. However setting this to true causes a mass rebuild
-      # of the *entire* system closure, so it must be handled with proper care.
-      enableParallelBuildingByDefault = false;
 
       # List of derivation warnings to display while rebuilding.
       #  See: <https://github.com/NixOS/nixpkgs/blob/master/pkgs/stdenv/generic/check-meta.nix>
