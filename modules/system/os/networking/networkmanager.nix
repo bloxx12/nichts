e811bea33d40fa0fbc2d3fdf298cce21ddd92926
Vali <valentin@kaas.cc>
2024-08-16 13:32:13 +0200

    system: add config for networking and hardware

---
+++ modules/system/os/networking/networkmanager.nix
@@ -0,0 +1,26 @@
+{lib, ...}: let
+  inherit (lib) mkForce;
+in {
+  networking.networkmanager = {
+    enable = true;
+    # Removes about 2GB of stuff we do no need.
+    plugins = mkForce [];
+
+    dns = "systemd-resolved";
+    unmanaged = [
+      # DO NOT manage my docker containers, thank you.
+      "interface-name:docker*"
+    ];
+    wifi = {
+      # Generate a randomized value upon each connect
+      macAdress = "random";
+
+      # Enable Wi-Fi power saving
+      powersave = true;
+
+      # Backend is either wpa_supplicant or iwd,
+      # we use wpa_supplicant.
+      backend = "wpa_supplicant";
+    };
+  };
+}
