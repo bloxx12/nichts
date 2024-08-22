2862aa7aa9b77060c7d68af7dc28760599b6f719
Charlie Root <charlie@charlieroot.dev>
2024-08-22 21:26:47 +0200

    removed nixos-hardware

--- hosts/vali/hermit/hardware-configuration.nix
+++ hosts/vali/hermit/hardware-configuration.nix
@@ -15,6 +15,7 @@
   boot.initrd.availableKernelModules = ["xhci_pci" "ahci" "nvme" "usb_storage" "sd_mod" "rtsx_pci_sdmmc"];
   boot.initrd.kernelModules = [];
   boot.kernelModules = ["kvm-intel"];
+  boot.kernelParams = ["mem_sleep_default=deep"];
   boot.extraModulePackages = [];
 
   fileSystems."/" = {
@@ -32,13 +33,17 @@
   #  swapDevices =
   #    [ { device = "/dev/disk/by-uuid/3518272e-1051-41e2-a7f0-f5c744e46789"; }
   #    ];
-
+  # Earlier font setup
+  console.earlySetup = true;
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
   # networking.interfaces.wlp59s0.useDHCP = lib.mkDefault true;
+
+  # Enable firmware updates via `fwupdmgr`.
+  services.fwupd.enable = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
