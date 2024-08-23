afbc68411dd66e9e83544a6b83309558eaca9ec3
Charlie Root <charlie@charlieroot.dev>
2024-08-23 08:22:22 +0200

    nvf: fix autocomplete, tabline

--- modules/programs/editors/nvf/plugins/settings/autocomplete.nix
+++ modules/programs/editors/nvf/plugins/settings/autocomplete.nix
@@ -4,8 +4,6 @@ _: {
       enable = true;
       type = "nvim-cmp";
       mappings = {
-        close = "<C-e>";
-        confirm = "<tab>";
         next = "<C-n>";
         previous = "<C-p>";
         scrollDocsDown = "<C-j>";
