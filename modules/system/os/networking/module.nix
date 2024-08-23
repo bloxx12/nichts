070a4d86756250e35e0c78b15146089034dbbc55
Charlie Root <charlie@charlieroot.dev>
2024-08-23 08:23:14 +0200

    networking.nix: fix hostname

--- modules/system/os/networking/module.nix
+++ modules/system/os/networking/module.nix
@@ -1,11 +1,11 @@
 {config, ...}: let
-  inherit (config.modules.other.system) username;
+  inherit (config.modules.other.system) hostname username;
 in {
   imports = [
     ./networkmanager.nix
   ];
   networking = {
-    hostName = username;
+    hostName = hostname;
     enableIPv6 = true;
     nameservers = [
       # quad9 DNS
