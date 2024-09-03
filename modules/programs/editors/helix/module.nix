4d66f4f332b0e032523b61cf69a25ff4d955a618
Charlie Root <charlie@charlieroot.dev>
2024-09-03 15:20:49 +0200

    helix/module.nix: setting changes

--- modules/programs/editors/helix/module.nix
+++ modules/programs/editors/helix/module.nix
@@ -7,7 +7,7 @@
 }: let
   cfg = config.modules.system.programs.editors.helix;
   inherit (config.modules.other.system) username;
-  inherit (lib) mkIf getExe makeBinPath;
+  inherit (lib) mkIf makeBinPath;
 in {
   imports = [./languages.nix];
   config = mkIf cfg.enable {
@@ -41,12 +41,14 @@ in {
             lsp.display-inlay-hints = true;
             line-number = "relative";
             true-color = true;
+            auto-format = true;
+            completion-timeout = 5;
             mouse = true;
             bufferline = "multiple";
             soft-wrap.enable = true;
             lsp.display-messages = true;
             cursor-shape = {insert = "bar";};
-            statusline.left = ["mode" "spinner" "version-control" "file-name"];
+            statusline.left = ["spinner" "version-control" "file-name"];
             inline-diagnostics = {
               cursor-line = "hint";
               other-lines = "error";
