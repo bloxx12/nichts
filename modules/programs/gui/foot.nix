d2be95447fa60772c0609c2f36d45275c958f61e
Charlie Root <charlie@charlieroot.dev>
2024-09-03 15:20:35 +0200

    foot: settings changes

--- modules/programs/gui/foot.nix
+++ modules/programs/gui/foot.nix
@@ -17,28 +17,18 @@ in {
         package = pkgs.foot;
         settings = {
           main = {
-            term = "foot";
+            term = "xterm-256color";
             app-id = "foot";
             title = "foot";
             locked-title = "no";
+            shell = "fish";
 
-            font = "ComicShannsMono Nerd Font:size=14";
+            font = "Iosevka Nerd Font:size=14";
+            font-bold = "Iosevka Nerd Font:size=14";
 
-            # line-height = 20;
-            letter-spacing = 0;
-            horizontal-letter-offset = 0;
-            vertical-letter-offset = -0.75;
-            box-drawings-uses-font-glyphs = "no";
-            dpi-aware = "no";
+            dpi-aware = false;
 
-            initial-window-size-chars = "104x36";
-            initial-window-mode = "windowed";
-            pad = "5x5 center";
-            resize-delay-ms = 100;
-
-            bold-text-in-bright = "no";
-            word-delimiters = '',│`|:"'()[]{}<>'';
-            selection-target = "primary";
+            pad = "0x0 center";
           };
 
           desktop-notifications.command = "${pkgs.libnotify}/bin/notify-send -a \${app-id} -i \${app-id} \${title} \${body}";
@@ -51,7 +41,7 @@ in {
           };
 
           scrollback = {
-            lines = 100000;
+            lines = 10000;
             multiplier = 10.0;
             indicator-position = "relative";
             indicator-format = "line";
@@ -66,21 +56,18 @@ in {
               abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_.,~:;/?#@!$&%*+="'()[]'';
           };
 
+          tweak = {
+            font-monospace-warn = "no";
+            sixel = "yes";
+          };
           cursor = {
             style = "beam";
             blink = "true";
           };
 
           mouse = {
-            hide-when-typing = "yes"; # not really needed since we already enable this in Hyprland
+            hide-when-typing = "yes"; # not really needed since we already enable this in Hyprland.
             alternate-scroll-mode = "yes";
-          };
-
-          csd = {preferred = "server";};
-
-          key-bindings = {
-            show-urls-launch = "Control+Shift+u";
-            unicode-input = "Control+Shift+i";
           };
         };
       };
