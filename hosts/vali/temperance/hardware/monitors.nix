65fa47449b4e880254f2b2213013ebd96bf8998c
Charlie Root <charlie@charlieroot.dev>
2024-09-04 11:10:43 +0200

    temperance/hardware: init, add monitors

---
+++ hosts/vali/temperance/hardware/monitors.nix
@@ -0,0 +1,40 @@
+_: {
+  modules.system.hardware.monitors = {
+    DP-2 = {
+      resolution = {
+        x = 1920;
+        y = 1080;
+      };
+      scale = 1;
+      refreshRate = 60;
+      position = {
+        x = 0;
+        y = 0;
+      };
+    };
+    HDMI-A-2 = {
+      resolution = {
+        x = 1920;
+        y = 1080;
+      };
+      scale = 1;
+      refreshRate = 60;
+      position = {
+        x = 1920;
+        y = 0;
+      };
+    };
+    HDMI-A-1 = {
+      resolution = {
+        x = 1920;
+        y = 1080;
+      };
+      scale = 1;
+      refreshRate = 60;
+      position = {
+        x = 3840;
+        y = 0;
+      };
+    };
+  };
+}
