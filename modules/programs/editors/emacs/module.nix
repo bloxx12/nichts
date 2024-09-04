75691b6263ca0ffaa1c238f3a2002ee1e8b449bf
Charlie Root <charlie@charlieroot.dev>
2024-09-04 22:59:53 +0200

    emacs: finish changes, migrated to nixxed doom emacs

--- modules/programs/editors/emacs/module.nix
+++ modules/programs/editors/emacs/module.nix
@@ -26,6 +26,12 @@
       with epkgs; [
         treesit-grammars.with-all-grammars
         vterm
+        eshell-prompt-extras
+        esh-autosuggest
+        fish-completion
+        esh-help
+        eshell-syntax-highlighting
+        pinentry
       ]));
 in {
   config = mkIf cfg.enable {
@@ -33,56 +39,42 @@ in {
       imports = [inputs.nix-doom-emacs-unstraightened.hmModule];
       programs.doom-emacs = {
         enable = true;
-        # doomDir = ./doom;
+        doomDir = ./doom;
         emacs = custom-emacs;
-        extraBinPackages = with pkgs; [git python3 pinentry-tty];
-        extraPackages = epkgs:
-          with epkgs; [
-            vterm
-            treesit-grammars.with-all-grammars
-            eshell-prompt-extras
-            esh-autosuggest
-            fish-completion
-            esh-help
-            eshell-syntax-highlighting
-            pinentry
-          ];
-        provideEmacs = true;
-        # experimentalFetchTree = true;
+        extraBinPackages = with pkgs; [
+          python3
+          pinentry-tty
+          pinentry-emacs
+          # needed by native-comp
+          binutils
+
+          # Doom dependencies
+          git
+          ripgrep
+          gnutls
+
+          ## Optional dependencies
+          fd # faster projectile indexing
+          imagemagick # for image-dired
+          zstd # for undo-fu-session/undo-tree compression
+          nodePackages.prettier
+
+          # Module dependencies
+          # spell
+          (aspellWithDicts (ds: with ds; [de en en-computers en-science]))
+          # editorconfig
+          editorconfig-core-c # per-project style config
+          # lookup & org +roam
+          sqlite
+          # latex & org (latex previews)
+          texlive.combined.scheme-medium
+          # :lang beancount
+          # beancount
+          # fava
+          # :lang nix
+          age
+        ];
       };
-      home.packages = with pkgs; [
-        # needed my native-comp
-        binutils
-        # Emacs itself
-        custom-emacs
-
-        # Doom dependencies
-        git
-        ripgrep
-        gnutls
-
-        ## Optional dependencies
-        fd # faster projectile indexing
-        imagemagick # for image-dired
-        # (mkIf (config.programs.gnupg.agent.enable)
-        #   pinentry-emacs) # in-emacs gnupg prompts
-        zstd # for undo-fu-session/undo-tree compression
-        nodePackages.prettier
-        # Module dependencies
-        # :checkers spell
-        (aspellWithDicts (ds: with ds; [de en en-computers en-science]))
-        # :tools editorconfig
-        editorconfig-core-c # per-project style config
-        # :tools lookup & :lang org +roam
-        sqlite
-        # :lang latex & :lang org (latex previews)
-        texlive.combined.scheme-medium
-        # :lang beancount
-        # beancount
-        # fava
-        # :lang nix
-        age
-      ];
     };
   };
 }
