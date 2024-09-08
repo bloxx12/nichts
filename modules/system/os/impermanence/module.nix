b05f564dd98f95a8e8a560ce983a15eba745c822
Charlie Root <charlie@charlieroot.dev>
2024-09-08 20:54:32 +0200

    imerpanence/module.nix: disable

--- modules/system/os/impermanence/module.nix
+++ modules/system/os/impermanence/module.nix
@@ -8,23 +8,25 @@
 
   cfg = config.modules.system.impermanence;
 in {
-  imports = [
-    inputs.impermanence.nixosModules.impermanence
-  ];
-  environment.persistence."/persist" = {
-    directories = [
-      "/etc/nixos"
-      "/etc/nix"
-      "/etc/NetworkManager/system-connections"
-      "/var/db/sudo"
-      "/var/lib/bluetooth"
-      "/var/lib/nixos"
-      "/var/lib/pipewire"
-      "/var/lib/systemd/coredump"
-    ];
+  # config = mkIf false {
+  #   imports = [
+  #     inputs.impermanence.nixosModules.impermanence
+  #   ];
+  #   environment.persistence."/persist" = {
+  #     directories = [
+  #       "/etc/nixos"
+  #       "/etc/nix"
+  #       "/etc/NetworkManager/system-connections"
+  #       "/var/db/sudo"
+  #       "/var/lib/bluetooth"
+  #       "/var/lib/nixos"
+  #       "/var/lib/pipewire"
+  #       "/var/lib/systemd/coredump"
+  #     ];
 
-    files = [
-      "/etc/machine-id"
-    ];
-  };
+  #     files = [
+  #       "/etc/machine-id"
+  #     ];
+  #   };
+  # };
 }
