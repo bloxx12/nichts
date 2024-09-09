c77cb98416cc9a1663b295a9fbffbd1f2239fb7d
Charlie Root <charlie@charlieroot.dev>
2024-09-09 21:33:26 +0200

    emacs/module.nix: remove extra packages

--- modules/programs/editors/emacs/module.nix
+++ modules/programs/editors/emacs/module.nix
@@ -17,77 +17,12 @@
       package
     ]);
 
-  custom-emacs = with newpkgs; ((emacsPackagesFor (emacs-pgtk.override {
-      withNativeCompilation = true;
-      withTreeSitter = true;
-    }))
+  custom-emacs = with newpkgs;
+    (emacsPackagesFor emacs-pgtk)
     .emacsWithPackages (epkgs:
       with epkgs; [
-        avy
-        better-jumper
-        catppuccin-theme
-        cape
-        corfu
-        consult
-        crux
-        cmake-font-lock
-        diminish
-        (trivialBuild {
-          pname = "on.el";
-          src = pkgs.fetchFromGitLab {
-            owner = "ajgrf";
-            repo = "on.el";
-            rev = "3cf623e1";
-            sha256 = "sha256-gtSVCpQwv4Ui9VpW7SXnsXIkfHN/6laMLqHTezDcMZg=";
-          };
-          version = "0.1.0";
-        })
-        direnv
-        dirvish
-        doom-modeline
-        editorconfig
-        face-explorer
-        flycheck
-        fontaine
-        frames-only-mode
-        fussy
-        gcmh
-        groovy-mode
-        just-mode
-        kotlin-mode
-        lsp-mode
-        lsp-treemacs
-        lsp-ui
-        magit
-        markdown-mode
-        meow
-        meow-tree-sitter
-        mode-line-bell
-        nasm-mode
-        nix-mode
-        reformatter # required by nix mode
-
-        projectile
-        peep-dired
-        persist-state
-        rainbow-mode
-        no-littering
-        suggest
-        string-inflection
-        tldr
-        treesit-auto
-        treesit-grammars.with-all-grammars
-        treemacs
-        treemacs-projectile
-        treemacs-magit
-        undo-tree
-        use-package
-        vertico
         vterm
-        which-key
-        ws-butler
-        zoom
-      ]));
+      ]);
 in {
   config = mkIf cfg.enable {
     home-manager.users.${username} = {
