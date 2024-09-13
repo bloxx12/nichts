f32c3cf872a5fe1eeaab4662fc92e38f03e4da1b
Charlie Root <charlie@charlieroot.dev>
2024-09-13 19:10:47 +0200

    README: update, add logo

---
+++ parts/pkgs/extraPackages/nushell/default.nix
@@ -0,0 +1,29 @@
+{
+  inputs,
+  lib,
+  pkgs,
+  ...
+}: let
+  inherit (lib) concatStringsSep mapAttrsToList;
+  aliases = import ./aliases.nix {inherit pkgs;};
+
+  aliasesAsString =
+    concatStringsSep "\n"
+    (mapAttrsToList (k: v: "alias ${k}=\"${v}\"") aliases);
+  packages = import ./packages.nix {inherit pkgs;};
+
+  nushell-wrapped = inputs.wrapper-manager.lib.build {
+    inherit pkgs;
+    modules = [
+      {
+        wrappers.nushell-wrapped = {
+          basePackage = pkgs.nushell;
+          pathAdd = [
+            packages
+          ];
+        };
+      }
+    ];
+  };
+in
+  nushell-wrapped
---
+++ parts/pkgs/extraPackages/nushell/packages.nix
@@ -0,0 +1,49 @@
+{pkgs}: (with pkgs; [
+  # better cd
+  zoxide
+  #better ls
+  eza
+
+  # better cat
+  bat
+
+  # better grep
+  ripgrep
+
+  # better dig
+  dogdns
+
+  # simply the best fetch tool out there
+  microfetch
+
+  fzf
+
+  cpc
+  difftastic
+  hexyl
+  gitui
+  iputils
+  gnumake
+  gping
+  asciinema
+  inetutils
+  scc
+  onefetch
+  wget
+  cpufetch
+  yt-dlp
+  tealdeer
+  glow
+  hyperfine
+  imagemagick
+  ffmpeg-full
+  catimg
+  nmap
+  wget
+  fd
+  jq
+  rsync
+  figlet
+  unzip
+  zip
+])
