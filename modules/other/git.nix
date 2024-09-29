b44d206b76f3d1b20b9432c5c415b845ac712747
Charlie Root <charlie@charlieroot.dev>
2024-09-29 23:13:13 +0200

    other/git.nix: add userName and userEmail

--- modules/other/git.nix
+++ modules/other/git.nix
@@ -8,6 +8,9 @@ in {
   home-manager.users.${username} = {
     home.packages = with pkgs; [gnupg];
     programs.git = {
+      enable = true;
+      userName = "Charlie Root";
+      userEmail = "charlie@charlieroot.dev";
       signing = {
         key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILPiRe9OH/VtWFWyy5QbAVcN7CLxr4zUtRCwmxD6aeN6";
         signByDefault = true;
