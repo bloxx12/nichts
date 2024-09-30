8606d029f79fdf8e10a59f4ae050c98602fcaf4a
Charlie Root <charlie@charlieroot.dev>
2024-09-30 21:50:19 +0200

    hardware/cpu/intel.nix: fix nix code

--- modules/system/hardware/cpu/intel.nix
+++ modules/system/hardware/cpu/intel.nix
@@ -9,7 +9,7 @@
 in {
   hardware = {
     cpu.intel.updateMicrocode = mkDefault config.hardware.enableRedistributableFirmware;
-    hardware.graphics.extraPackages = with pkgs; [
+    graphics.extraPackages = with pkgs; [
       intel-vaapi-driver
       intel-media-driver
     ];
