b66e585254dfad7a3379267d1650de0be2a5c277
Charlie Root <charlie@charlieroot.dev>
2024-10-20 01:25:54 +0200

    quickshell.nix: override default package

--- modules/style/quickshell/module.nix
+++ modules/style/quickshell/module.nix
@@ -13,7 +13,14 @@ in {
     home.packages = with pkgs; [
       qt6.qtimageformats # amog
       qt6.qt5compat # shader fx
-      quickshell.packages.default
+      (quickshell.packages.default.override {
+        withJemalloc = true;
+        withQtSvg = true;
+        withX11 = true;
+        withPipewire = true;
+        withPam = true;
+        withHyprland = true;
+      })
       pamtester # lockscreen
       grim
       imagemagick # screenshot
