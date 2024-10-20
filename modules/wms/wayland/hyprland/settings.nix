4ff9cfe7e9db7aec06b3f657479d4f02b6427d47
Charlie Root <charlie@charlieroot.dev>
2024-10-20 23:26:05 +0200

    hyprland/settings.nix: enable caps escape key

--- modules/wms/wayland/hyprland/settings.nix
+++ modules/wms/wayland/hyprland/settings.nix
@@ -34,7 +34,7 @@ in {
       input = {
         kb_layout = "de,ru, us";
         kb_variant = ",phonetic_winkeys,workman";
-        kb_options = "grp:rctrl_rshift_toggle caps:escape";
+        kb_options = "grp:rctrl_rshift_toggle, caps:escape";
 
         follow_mouse = true;
 
