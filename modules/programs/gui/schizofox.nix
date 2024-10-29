4b892471207a4b66bc436f53c206844d52c746eb
Charlie Root <charlie@charlieroot.dev>
2024-10-29 11:44:16 +0100

    schizofox.nix: fix deprecated sanitizeOnShutdown

--- modules/programs/gui/schizofox.nix
+++ modules/programs/gui/schizofox.nix
@@ -160,7 +160,7 @@ in {
         };
 
         security = {
-          sanitizeOnShutdown = false;
+          sanitizeOnShutdown.enable = false;
           sandbox = false;
           noSessionRestore = false;
           userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:106.0) Gecko/20100101 Firefox/106.0";
