1a2284898578f4f76d9edf8fdfa54fafb18bc1aa
Charlie Root <charlie@charlieroot.dev>
2024-10-29 18:07:57 +0100

    steam: remove package config

--- modules/programs/gui/steam.nix
+++ modules/programs/gui/steam.nix
@@ -10,7 +10,7 @@ in {
   config = mkIf cfg.enable {
     programs.steam = {
       enable = true;
-      package = pkgs.steam;
+      # package = pkgs.steam;
       
       # .override {
       #   extraEnv = {
