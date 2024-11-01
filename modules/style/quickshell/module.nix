fb4ebc9f580950fc3248b8d5e0d52cf8752059c5
Charlie Root <charlie@charlieroot.dev>
2024-11-01 20:55:02 +0100

    quickshell/module.nix: move from home.packges to environment.systemPackages

--- modules/style/quickshell/module.nix
+++ modules/style/quickshell/module.nix
@@ -7,28 +7,20 @@
 }: let
   inherit (inputs) quickshell;
   inherit (lib.generators) toKeyValue;
-  inherit (config.meta.mainUser) username;
 in {
-  home-manager.users.${username} = {
-    home.packages = with pkgs; [
-      qt6.qtimageformats # amog
-      qt6.qt5compat # shader fx
-      (quickshell.packages.x86_64-linux.default.override {
-        withJemalloc = true;
-        withQtSvg = true;
-        withX11 = true;
-        withPipewire = true;
-        withPam = true;
-        withHyprland = true;
-      })
-      pamtester # lockscreen
-      grim
-      imagemagick # screenshot
-    ];
-
-    # xdg.configFile."quickshell/manifest.conf".text = toKeyValue {} {
-    #   shell = "${./shell}";
-    #   # lockscreen = "${./lockscreen}";
-    # };
-  };
+  environment.systemPackages = with pkgs; [
+    qt6.qtimageformats # amog
+    qt6.qt5compat # shader fx
+    (quickshell.packages.x86_64-linux.default.override {
+      withJemalloc = true;
+      withQtSvg = true;
+      withX11 = true;
+      withPipewire = true;
+      withPam = true;
+      withHyprland = true;
+    })
+    pamtester # lockscreen
+    grim
+    imagemagick # screenshot
+  ];
 }
