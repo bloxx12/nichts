89234cfa5af339139af7b0714e295be2db664d51
Charlie Root <charlie@charlieroot.dev>
2024-11-03 15:29:38 +0100

    user: add helix package

--- modules/user/default.nix
+++ modules/user/default.nix
@@ -1,11 +1,11 @@
 {
-  inputs,
   pkgs,
   ...
 }: rec {
   packages = {
     fish = pkgs.callPackage ./shell {};
-    helix = pkgs.callPackge ./helix {};
+    # helix = pkgs.callPackge ./helix {};
+    kakoune = pkgs.callPackage ./kakoune {};
   };
   shell = pkgs.mkShell {
     name = "bloxx-shell";
