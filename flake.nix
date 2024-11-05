129dcd0a4f9c3918aa75e4c2b589697e17ce10ca
Charlie Root <charlie@charlieroot.dev>
2024-11-06 00:15:51 +0100

    down to 4 flake inputs

--- flake.nix
+++ flake.nix
@@ -23,38 +23,11 @@
     };
     nixosModules = {
       user = user.module;
-      # shell = import ./modules/shell {inherit pkgs;};
     };
   };
   inputs = {
     # Unstable nixpkgs baby!
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-
-    # Lix, a faster nix fork.
-    lix-module = {
-      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.91.0.tar.gz";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    # Hyprland, my main compositor
-    hyprland.url = "github:hyprwm/Hyprland";
-
-    # Plugins for hyprland
-    hyprland-plugins = {
-      url = "github:hyprwm/hyprland-plugins";
-      inputs.hyprland.follows = "hyprland";
-    };
-
-    # Plugin to get split monitor workspaces
-    split-monitor-workspaces = {
-      url = "github:Duckonaut/split-monitor-workspaces";
-      inputs.hyprland.follows = "hyprland";
-    };
-
-    hyprsplit = {
-      url = "github:shezdy/hyprsplit";
-      inputs.hyprland.follows = "hyprland";
-    };
 
     impermanence.url = "github:nix-community/impermanence";
 
@@ -69,11 +42,6 @@
 
     spicetify-nix = {
       url = "github:gerg-l/spicetify-nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    quickshell = {
-      url = "git+https://git.outfoxxed.me/outfoxxed/quickshell";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
