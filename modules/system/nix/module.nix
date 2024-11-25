1d7478c156cae5a033307eaecf0275b2831661ae
Charlie Root <charlie@charlieroot.dev>
2024-11-25 14:54:15 +0100

    nix/module.nix: remove unneeded trusted public keys

--- modules/system/nix/module.nix
+++ modules/system/nix/module.nix
@@ -138,15 +138,11 @@ in {
       # Substituters to pull from.
       substituters = [
         "https://cache.nixos.org" # funny binary cache
-        "https://nix-community.cachix.org" # nix-community cache
-        "https://hyprland.cachix.org" # hyprland
         "https://helix.cachix.org" # a chache for helix
       ];
 
       trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
         "helix.cachix.org-1:ejp9KQpR1FBI2onstMQ34yogDm4OgU2ru6lIwPvuCVs="
       ];
     };
