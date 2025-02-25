8cc7f0f9cd6235ef69cdaebd26bd86d229d504d8
Charlie Root <charlie@charlieroot.dev>
2025-02-25 17:28:07 +0100

    os/systemd/module.nix: init, add faster boot options

---
+++ modules/system/os/systemd/module.nix
@@ -0,0 +1,7 @@
+{pkgs, ...}: {
+  config.systemd = {
+  # faster startup
+    targets.network-online.wantedBy = pkgs.lib.mkForce []; # Normally ["multi-user.target"]
+    services.NetworkManager-wait-online.wantedBy = pkgs.lib.mkForce []; # Normally ["network-online.target"]
+  };
+}
