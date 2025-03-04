265c5bbc40d4cfea216d52a3c3c54c4a9bdeaf4d
Charlie Root <charlie@charlieroot.dev>
2025-03-04 19:37:51 +0100

    flake.nix: working multi-system config

--- flake.nix
+++ flake.nix
@@ -2,6 +2,7 @@
   description = "My NixOS config flake";
   outputs = inputs: let
     pkgs = inputs.nixpkgs.legacyPackages.x86_64-linux;
+    user = import ./modules/user {inherit pkgs;};
     eachSystem = inputs.nixpkgs.lib.genAttrs (import inputs.systems);
     pkgsFor = inputs.nixpkgs.legacyPackages;
   in {
@@ -10,12 +11,20 @@
     devShells.x86_64-linux.default = pkgs.callPackage ./shell.nix {};
 
     formatter.x86_64-linux = pkgs.alejandra;
+    # packages.x86_64-linux = {inherit (user) packages;};
 
     packages = eachSystem (
       system: {
         inherit (import ./packages pkgsFor.${system}) helix fish;
       }
     );
+    # packages = eachSystem (
+    #   system: let
+    #     user = import ./modules/user {pkgs = pkgsFor.${system};};
+    #   in {
+    #     inherit (user) packages;
+    #   }
+    # );
 
     apps = eachSystem (system: let
       user = import ./modules/user {pkgs = pkgsFor.${system};};
@@ -34,6 +43,7 @@
     # in {
     #   user = user.module;
     # });
+    # nixosModules.user = user.module;
   };
   inputs = {
     # Unstable nixpkgs baby!
