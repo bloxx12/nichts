bb45d93cb5e0130379007a334fdb2e6e92bc10b7
Charlie Root <charlie@charlieroot.dev>
2025-03-04 22:32:40 +0100

    ssh.nix: accept ecdsa

--- modules/services/ssh.nix
+++ modules/services/ssh.nix
@@ -4,6 +4,11 @@ in {
   options.modules.programs.ssh.enable = mkEnableOption "ssh";
   config = {
     programs.ssh.startAgent = true;
+    programs.ssh.pubkeyAcceptedKeyTypes = [
+      "ssh-rsa"
+      "ecdsa-sha2-nistp521"
+    ];
+
     services.openssh = {
       enable = true;
       ports = [8997];
