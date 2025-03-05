8c2bfa6fcac0b3e1796483a56c9f77e0313a1f70
Charlie Root <charlie@charlieroot.dev>
2025-03-05 09:18:17 +0100

    ssh.nix: remove accepted public keys again

--- modules/services/ssh.nix
+++ modules/services/ssh.nix
@@ -4,10 +4,10 @@ in {
   options.modules.programs.ssh.enable = mkEnableOption "ssh";
   config = {
     programs.ssh.startAgent = true;
-    programs.ssh.pubkeyAcceptedKeyTypes = [
-      "ssh-rsa"
-      "ecdsa-sha2-nistp521"
-    ];
+    # programs.ssh.pubkeyAcceptedKeyTypes = [
+    #   "ssh-rsa"
+    #   "ecdsa-sha2-nistp521"
+    # ];
 
     services.openssh = {
       enable = true;
